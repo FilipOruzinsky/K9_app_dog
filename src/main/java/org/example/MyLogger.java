961dd70691a13b5c1c2a2fb1dc2059b1c9665536
FilipOruzinsky <oruzinskyfilip@gmail.com>
2023-09-21 12:00:24 +0200

    commit before try to implement separate logger for each class

--- src/main/java/org/example/MyLogger.java
+++ src/main/java/org/example/MyLogger.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MyLogger {
+    
     private static final Logger logger = LogManager.getLogger(MyLogger.class);
 
     public static void info(String message) {
